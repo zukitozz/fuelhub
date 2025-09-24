2e18c4805f968e4829049ac8ccf41e6ab869ac4e
xt9052 <xt9052@rimac.com.pe>
2025-09-24 18:03:09 -0500

    agregando guia validacion de campos

--- src/app/(billing)/invoice/StepB.tsx
+++ src/app/(billing)/invoice/StepB.tsx
@@ -132,7 +132,8 @@ const StepB: React.FC<StepBProps> = ({
       <div className='inline-flex items-center space-x-12 my-2 justify-between w-full'>
         <label htmlFor="gal_precio" className="block mb-2 text-lg font-large text-gray-900 dark:text-white">PRECIO</label>
         <input 
-          type="text" 
+          type="number" 
+          step="0.1" 
           name="gal_precio" 
           defaultValue={ '' }
           className="`block w-full px-4 py-3 text-lg text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`"
