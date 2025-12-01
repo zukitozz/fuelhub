8cb155a557a21873b1f57057482a90a9df3d6861
xt9052 <xt9052@rimac.com.pe>
2025-12-01 16:03:43 -0500

    config calllback url

--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -10,8 +10,6 @@ import { useUIStore } from '@/store';
 export const TopMenu = () => {
 
   const openSideMenu = useUIStore( state => state.openSideMenu );
-  console.log("hola");
-  console.log(process.env.NEXTAUTH_SECRET);
   return (
     <nav className="flex px-5 justify-between items-center w-full">
 
